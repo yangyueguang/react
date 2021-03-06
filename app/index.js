a1dfa4efcad5be680c14889d8e4f156b4b1035c6
yangyueguang <2829969299@qq.com>
2021-03-06 10:50:31 +0800

    update

--- app/index.js
+++ app/index.js
@@ -2,15 +2,15 @@ import 'babel-polyfill'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import { hot } from 'react-hot-loader/root'
 import '@config'
 import Routes from '@configs/router.config'
 import configure from '@middleware/configureStore'
 
+const HotRoutes = hot(Routes)
 const store = configure({ })
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Routes />
-  </Provider>,
+  <Provider store={store}><HotRoutes /></Provider>,
   document.getElementById('root'),
 )
